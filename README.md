17fb6cb8ba5ec846f3994c15c69d9396d96c0adc
Ross Lawley <ross.lawley@gmail.com>
2017-08-14 14:52:40 +0100

    concurrent Updates and Reads

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Various quick starts for using mongodb with java - check the different branches for different versions of the driver
 
 To run:  `./gradlew quickStart`
-or to supply a different connection string: `./gradlew quickStart -PconnectionString=mongodb://localhost`
+or to supply a different findAndModify setting: `./gradlew quickStart -DfindAndModify`
 
 Source code is in `./src/main/quickstart/QuickStart.java`
 Github source: https://github.com/rozza/mongo-java-quickstart
--- src/main/quickstart/QuickStart.java
+++ src/main/quickstart/QuickStart.java
@@ -16,50 +16,114 @@
 
 package quickstart;
 
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
-import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static com.mongodb.client.model.Filters.eq;
+import static java.lang.String.format;
 
 public final class QuickStart {
+
+    private static final Bson QUERY = eq("_id", 1);
+    private static final Map<Integer, String> entry = Collections.synchronizedMap(new LinkedHashMap<Integer, String>());
+    private static final Map<Integer, String> read = Collections.synchronizedMap(new LinkedHashMap<Integer, String>());
 
     /**
      * Run this main method to see the output of this quick example.
      *
      * ./gradlew quickStart
-     * ./gradlew quickStart -PconnectionString=mongodb://localhost
+     * ./gradlew quickStart -DfindAndModify=true
      *
-     * @param args takes an optional single argument for the connection string
+     * @param args takes an optional single argument for the findAndModify
      */
     public static void main(final String[] args) {
-        System.out.println("======= Start =======");
+        boolean findAndModify = args.length == 1 && Boolean.parseBoolean(args[0]);
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("test");
+        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("coll");
 
-        MongoClient mongoClient;
+        System.out.println("== Initialising the collection ==");
+        mongoCollection.drop();
+        Document original = Document.parse("{_id: 1, a: 'A_00', b: '00'}");
+        mongoCollection.insertOne(original);
 
-        if (args.length == 0) {
-            // connect to the local database server
-            mongoClient = new MongoClient();
-        } else {
-            mongoClient = new MongoClient(new MongoClientURI(args[0]));
+        System.out.println(format("Original document: %s",original.toJson()));
+        System.out.println(format("Using FindAndModify: %s", findAndModify));
+
+        ExecutorService executor = Executors.newFixedThreadPool(100);
+
+        for (int i = 0; i < 20; i++) {
+            Runnable worker = new WorkerThread(i, mongoCollection, findAndModify);
+            executor.execute(worker);
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+            // Busy loop
         }
 
-        // get handle to "mydb" database
-        MongoDatabase database = mongoClient.getDatabase("mydb");
+        System.out.println("\nThreads incoming order:\n--------------------------");
+        for (Map.Entry<Integer, String> m : entry.entrySet()) {
+            System.out.println(format("workerId: %02d | %s", m.getKey(), m.getValue()));
+        }
 
+        System.out.println("\nAfter DB Update- Records status:\n-------------------------------------");
+        for (Map.Entry<Integer, String> m : read.entrySet()) {
+            System.out.println(format("workerId: %02d | %s", m.getKey(), m.getValue()));
+        }
 
-        // get a handle to the "test" collection
-        MongoCollection<Document> collection = database.getCollection("test");
+        System.out.println("\nFinished");
+    }
 
-        System.out.println("Collection has " + collection.count() + " documents");
+    static class WorkerThread implements Runnable {
+        private final int workerId;
+        private final MongoCollection<Document> mongoCollection;
+        private final boolean findAndModify;
 
-        // close resources
-        mongoClient.close();
-        System.out.println("======= Finish =======");
+        WorkerThread(final int id, final MongoCollection<Document> mongoCollection, final boolean findAndModify){
+            this.workerId = id;
+            this.mongoCollection = mongoCollection;
+            this.findAndModify = findAndModify;
+        }
+
+        public void run() {
+            // search document where _id=1 and update it with new values
+            Document newDocument = new Document();
+
+            int k = workerId % 2;
+            if(k == 0) {
+                newDocument.append("a", format("A_%02d", workerId));
+            } else {
+                newDocument.append("b", format("%02d", workerId));
+            }
+
+            Document updateObj = new Document("$set", newDocument);
+
+            entry.put(workerId, format("%s", format("Updating: %s", updateObj.toJson())));
+
+            if (findAndModify) {
+                Document updated = mongoCollection.findOneAndUpdate(QUERY, updateObj,
+                        new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER));
+                read.put(workerId, updated.toJson());
+            } else {
+                mongoCollection.updateOne(QUERY, updateObj);
+                mongoCollection.find(QUERY).forEach((Block<Document>) document -> {
+                    read.put(workerId, document.toJson());
+                });
+            }
+
+        }
 
     }
 
